8c201579274ba44bf13df89c521a054467b9b9c1
Matt Dziuban <mrdziuban@gmail.com>
2015-08-04 09:47:55 -0400

    Update existing callbacks spec, add new spec.

--- spec/uploader/callback_spec.rb
+++ spec/uploader/callback_spec.rb
@@ -8,17 +8,46 @@ describe CarrierWave::Uploader do
     @uploader_class_1 = Class.new(CarrierWave::Uploader::Base)
 
     # First Uploader only has default before-callback
-    expect(@uploader_class_1._before_callbacks[:cache]).to eq([:check_whitelist!, :check_blacklist!, :check_size!, :process!])
+    expect(@uploader_class_1._before_callbacks[:cache]).to eq([
+      [:check_whitelist!, []],
+      [:check_blacklist!, []],
+      [:check_size!, []],
+      [:process!, []]
+    ])
 
     @uploader_class_2 = Class.new(CarrierWave::Uploader::Base)
     @uploader_class_2.before :cache, :before_cache_callback
 
     # Second Uploader defined with another callback
-    expect(@uploader_class_2._before_callbacks[:cache]).to eq([:check_whitelist!, :check_blacklist!, :check_size!, :process!, :before_cache_callback])
+    expect(@uploader_class_2._before_callbacks[:cache]).to eq([
+      [:check_whitelist!, []],
+      [:check_blacklist!, []],
+      [:check_size!, []],
+      [:process!, []],
+      [:before_cache_callback, []]
+    ])
 
     # Make sure the first Uploader doesn't inherit the same callback
-    expect(@uploader_class_1._before_callbacks[:cache]).to eq([:check_whitelist!, :check_blacklist!, :check_size!, :process!])
+    expect(@uploader_class_1._before_callbacks[:cache]).to eq([
+      [:check_whitelist!, []],
+      [:check_blacklist!, []],
+      [:check_size!, []],
+      [:process!, []]
+    ])
   end
 
+  it "allows passing arguments to callbacks" do
+    @uploader_class_1 = Class.new(CarrierWave::Uploader::Base)
+
+    @uploader_class_1.before :cache, :before_cache_callback, 'arg1', 'arg2'
+
+    expect(@uploader_class_1._before_callbacks[:cache]).to eq([
+      [:check_whitelist!, []],
+      [:check_blacklist!, []],
+      [:check_size!, []],
+      [:process!, []],
+      [:before_cache_callback, ['arg1', 'arg2']]
+    ])
+  end
 
 end
